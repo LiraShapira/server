46c2ca321d21777466d704a06b987e3fad2d5318
Eldar <loshinas@gmail.com>
2025-09-20 13:57:39 +0300

    revert index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,8 @@
 import app from './server';
+const PORT = process.env.PORT || 3001;
 import { PrismaClient } from '@prisma/client'
-
-const PORT = Number(process.env.PORT) || 3001;
 export const prisma = new PrismaClient()
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`server available on http://0.0.0.0:${PORT}/`);
+app.listen(PORT, () => {
+  console.log(`server available on http://localhost:${PORT}/`);
 });
